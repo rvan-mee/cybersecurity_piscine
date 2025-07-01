cf988d85cef764aece71ca300fc4805d8550bd44
rvan-mee <ruben.vanmeerten@hotmail.nl>
2025-07-01 19:40:59 +0200

    Fixes a couple of issues, adds extra headers to the requests so it can bypass some filters, changes some error logic

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from html.parser import HTMLParser
+from urllib import request, error
 from urllib.parse import urljoin
 from urllib.parse import urlparse
 from colorama import init as colorama_init
@@ -8,23 +9,37 @@ from colorama import Fore
 from colorama import Style
 import urllib.request
 import argparse
+import gzip
+import io
 import sys
 import os
 
 
 clear_prev_line = '\33[2K'
 
+anti_bot_headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Sec-Ch-Ua": "\"Google Chrome\";v=\"123\", \"Not:A-Brand\";v=\"8\", \"Chromium\";v=\"123\"",
+    "Referer":"https://www.google.com/",
+    "Sec-Ch-Ua-Platform": "\"Windows\"",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+}
+
 def parse_input():
     parser = argparse.ArgumentParser(prog='spider', usage='%(prog)s [options]', description='A script to download images from a given website')
     parser.add_argument('URL', help='The URL used to download the data from')
     parser.add_argument('-r', '--recursive', help='Enables recursive downloads', action='store_true')
-    parser.add_argument('-l', '--depth', nargs='?', type=int, help='Sets the depth of the recursion, if the -r flag is enabled and no value is given a default depth of 5 is set', const=5)
-    parser.add_argument('-p', '--path', nargs=1, type=str, help='Sets the path where the images will be downloaded to, the default directory being ./data/ which will be created if it does not exist already', default='./data/')
+    parser.add_argument('-l', '--depth', nargs='?', type=int, help='Sets the depth of the recursion, if the -r flag is enabled and no value is given a default depth of 5 is set', const=5, default=1)
+    parser.add_argument('-p', '--path', type=str, help='Sets the path where the images will be downloaded to, the default directory being ./data/ which will be created if it does not exist already', default='./data/')
 
     args = parser.parse_args()
 
     # Make sure recursion is enabled when the -l flag is selected
-    if not args.recursive and ('-l' or 'depth' in sys.argv):
+    if not args.recursive and ('-l' in sys.argv or 'depth' in sys.argv):
         print(Fore.RED + 'The -r or --recursive must be enabled when -l is given' + Fore.RESET)
         exit(1)
 
@@ -32,10 +47,9 @@ def parse_input():
         print(Fore.RED + 'Please provide a positive recursion depth' + Fore.RESET)
         exit(2)
 
-    # Check if the given path exists
-    if not os.path.isdir(args.path) and args.path != './data/':
-        print(Fore.RED + 'Please provide a valid download directory' + Fore.RESET)
-        exit(3)
+    # Ensure the URL starts with 'https://'
+    if not args.URL.startswith('http://') and not args.URL.startswith('https://'):
+        args.URL = 'https://' + args.URL
 
     return args
 
@@ -44,21 +58,18 @@ def check_dir_permissions(path):
     return os.access(path, os.W_OK)
 
 def check_download_dir(path):
-    # Create default directory if it does not exist
-    if path == './data/' and not os.path.isdir(path):
-        if not check_dir_permissions('./'):
-            print(Fore.RED + 'No permission to create the default ./data/ directory' + Fore.RESET)
-            exit(4)
-        else:
+    # Create directory if it does not exist
+    if not os.path.isdir(path):
+        try:
             os.mkdir(path)
-            return
+        except OSError as error:
+            print(Fore.RED + 'Error trying to create directory: ' + path + ': ' + error + Fore.RESET)
+        return
 
     # Check if there are write permissions for the given directory
     if not check_dir_permissions(path):
         print(Fore.RED + 'No write permissions for the given download path: ' + path + Fore.RESET)
-        exit(5)
-
-    return
+        exit(4)
 
 
 accepted_img_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
@@ -110,8 +121,12 @@ class SpiderParser(HTMLParser):
 
 def validate_url(url):
     try:
+        request_params = request.Request(
+            url=url, 
+            headers=anti_bot_headers
+        )    
         # Just make a request to the given url and see if it succeeds
-        urllib.request.urlopen(url)
+        request.urlopen(request_params)
     except Exception as error:
         print(clear_prev_line + Fore.RED + 'Invalid URL: ' + url, end='')
         print(' Error: ', end='')
@@ -140,7 +155,25 @@ def scrape_image_urls(current_url, recursion_enabled, depth):
 
     # Parse the current url's HTML
     print(clear_prev_line + Fore.LIGHTBLACK_EX + 'Crawling on url: ' + current_url + Fore.RESET, end='\r')
-    html = urllib.request.urlopen(current_url).read().decode('utf-8')
+    request_params = request.Request(
+        url=current_url, 
+        headers=anti_bot_headers
+    )
+    # html = urllib.request.urlopen(request_params).read().decode('utf-8')
+
+    with urllib.request.urlopen(request_params) as response:
+        # Check for Content-Encoding
+        content_encoding = response.headers.get('Content-Encoding', '')
+        
+        if 'gzip' in content_encoding:
+            # If the response is gzip-compressed, decompress it
+            buf = io.BytesIO(response.read())
+            with gzip.GzipFile(fileobj=buf) as f:
+                html = f.read().decode('utf-8')  # Decode after decompressing
+        else:
+            # Read the response and decode utf-8 directly
+            html = response.read().decode('utf-8')
+
     parser = SpiderParser()
     parser.feed(html)
 
@@ -162,6 +195,9 @@ def scrape_image_urls(current_url, recursion_enabled, depth):
 
 
 def get_image_filename(download_dir_path, image_url):
+    if not download_dir_path.endswith('/'):
+        download_dir_path += '/'
+
     filepath = download_dir_path + image_url.replace('/', '_')
 
     if not os.path.exists(filepath):
@@ -183,7 +219,7 @@ def download_images(download_dir_path):
             file_name = get_image_filename(download_dir_path, image_url)
             with open(file_name, 'wb') as image:
                 image.write(image_data.read())
-                print(Fore.GREEN + 'Downloaded image: ' + file_name + Fore.RESET)
+                print(Fore.GREEN + 'Downloaded image: ' + image_url + Fore.RESET)
 
 
 if __name__=="__main__":
@@ -191,7 +227,7 @@ if __name__=="__main__":
     args = parse_input()
     check_download_dir(args.path)
     if not validate_url(args.URL):
-        print('Please provide a valid url - have you added the scheme?')
-        exit(6)
+        print(clear_prev_line + Fore.RED + 'Please provide a valid url' + Fore.RESET)
+        exit(5)
     scrape_image_urls(args.URL, args.recursive, args.depth)
     download_images(args.path)
